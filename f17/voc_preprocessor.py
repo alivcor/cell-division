4c129c74cc4244ee8bb072c8ff7ce2f97e373094
alivcor <abhinandandubey@live.com>
2017-09-11 17:09:28 -0400

    voc

--- f17/voc_preprocessor.py
+++ f17/voc_preprocessor.py
@@ -3,7 +3,7 @@ from xml.dom import minidom
 
 def generateFileIDs(dataset_class_path):
     with open(dataset_class_path, "r") as f:
-        filtered_ids = []
+        class_of_interest_ids = []
         all_lines = f.readlines()
         for line in all_lines:
             try:
@@ -12,9 +12,9 @@ def generateFileIDs(dataset_class_path):
                 # print line
                 fileid, garbage, fileclass = line.strip().split(" ")
             if(int(fileclass) == 1):
-                filtered_ids.append(fileid)
-    print("Found " + str(len(filtered_ids)) + " files in this class.")
-    return filtered_ids
+                class_of_interest_ids.append(fileid)
+    print("Found " + str(len(class_of_interest_ids)) + " files in this class.")
+    return class_of_interest_ids
 
 def countObjects(file_ids, dataset_annotations_path):
     filtered_ids = []
@@ -22,12 +22,13 @@ def countObjects(file_ids, dataset_annotations_path):
         e = xml.etree.ElementTree.parse(dataset_annotations_path + anno_file + ".xml").getroot()
         xml_anno = minidom.parse(dataset_annotations_path + anno_file + ".xml")
         num_objects = xml_anno.getElementsByTagName('object')
-        print(anno_file + " : " + str(len(num_objects)))
+        # print(anno_file + " : " + str(len(num_objects)))
         if(len(num_objects) == 1):
             filtered_ids.append(anno_file)
+    print "Number of images with only class of interest : " + str(len(filtered_ids))
     return filtered_ids
 
 
-dataset_class_path = "VOC2007/ImageSets/Main/dog_train.txt"
+dataset_class_path = "VOC2007/ImageSets/Main/cat_val.txt"
 dataset_annotations_path = "VOC2007/Annotations/"
 print countObjects(generateFileIDs(dataset_class_path), dataset_annotations_path)
